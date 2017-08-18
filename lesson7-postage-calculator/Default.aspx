7c445ee757065669b1034f71d28a45ef3be904ed
Ted Dunn <tldunn451@gmail.com>
2017-08-18 12:08:31 -0400

    Add code to complete lesson 7 postage calculator challenge for Tech Academy C# course page 18.

---
+++ lesson7-postage-calculator/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="lesson7_postage_calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Names="Arial" Font-Size="X-Large" Text="Postal Calculator"></asp:Label>
+            <br />
+            <br />
+            Width:&nbsp;&nbsp;
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged" Width="35px"></asp:TextBox>
+            <br />
+            Height:&nbsp;
+            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged" Width="35px"></asp:TextBox>
+            <br />
+            Length:&nbsp;
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged" Width="35px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" Checked="True" GroupName="shippingGroup" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="shippingGroup" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="shippingGroup" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ lesson7-postage-calculator/Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lesson7_postage_calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private double GROUND_SHIPPING_MULTIPLIER = 0.15;
+        private double AIR_SHIPPING_MULTIPLIER = 0.25;
+        private double NEXT_DAY_SHIPPING_MULTIPLIER = 0.45;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));           
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            displayPostageCost(calculatePostageCost(validateData(), determineShippingMultiplier()));
+        }
+
+        private double[] validateData()
+        {
+            double[] numberArray = new double[3];
+            double width;
+            double height;
+            double length;
+
+            bool widthValid = double.TryParse(widthTextBox.Text.Trim(), out width);
+            bool heightValid = double.TryParse(heightTextBox.Text.Trim(), out height);
+            bool lengthValid = double.TryParse(lengthTextBox.Text.Trim(), out length);
+
+            if(!lengthValid)
+            {
+                length = 1.0;
+                lengthValid = true;
+            }
+
+            if (widthValid && heightValid && lengthValid)
+            {
+                numberArray[0] = width;
+                numberArray[1] = height;
+                numberArray[2] = length;
+            }
+
+            return numberArray;
+        }
+
+        private double determineShippingMultiplier()
+        {
+            if (groundRadioButton.Checked)
+                return GROUND_SHIPPING_MULTIPLIER;
+            else if (airRadioButton.Checked)
+                return AIR_SHIPPING_MULTIPLIER;
+            else
+                return NEXT_DAY_SHIPPING_MULTIPLIER;
+        }
+
+        private double calculatePostageCost(double[] numArray, double shippingMultiplier)
+        {
+            double postageCost = numArray[0] * numArray[1] * numArray[2] * shippingMultiplier;
+
+            return postageCost;
+        }
+
+        private void displayPostageCost(double postageCost)
+        {
+            if (postageCost != 0)
+                resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", postageCost);
+            else
+                resultLabel.Text = "";
+        }
+    }
+}
